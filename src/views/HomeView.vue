a31c545417137d7f1b37cfc2ff2e55d0b1018630
NASER ADDIN Dany <naser.dany.0795@gmail.com>
2025-01-25 10:40:21 -0500

    setup basic buy button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,49 @@
 <script setup lang="ts">
+import { createClient, useArcpay } from "arcpay-sdk";
+import { ref, onMounted } from "vue";
+const apiKey = import.meta.env.VITE_PRIVATE_API_KEY;
+
+onMounted(() => {
+  // Instantiate the arcpay client singleton for a given network. Calling this method twice with the same network will return the same instance.
+  createClient("algo:testnet", {
+    apiKey: apiKey, // API key can be obtained from the arcpay dashboard
+  });
+});
+
+const handleEvent = async () => {
+  console.log("blabla");
+  // You can use the useArcpay helper to get the client instance after instantiating it.
+  const client = useArcpay("algo:testnet");
+  console.log(client);
+
+  console.log(await client.getListings());
+
+  // Create a VOI -> ARC-72 sale
+  const listingId: string = await client.create({ listingType: "sale" });
+  console.log(listingId);
+
+  // Get all listings for your account
+  const listings = await client.getListings();
+  console.log(listings);
+};
 </script>
+
 <template>
-  <div style="background-color: var(--color-mint-500)">
-    <h1 class="text-8xl">Test</h1>
-  </div>
-  <div
-    class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5"
-  >
-    <div>
-      <span
-        class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg"
+  <section class="text-gray-200 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div
+        class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto"
       >
-      </span>
+        <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font">
+          Sample dummy test
+        </h1>
+        <button
+          @click="handleEvent"
+          class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0"
+        >
+          Create a Sale
+        </button>
+      </div>
     </div>
-    <h3
-      class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight"
-    >
-      Writes upside-down
-    </h3>
-    <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm">
-      The Zero Gravity Pen can be used to write in any orientation, including
-      upside-down. It even works in outer space.
-    </p>
-  </div>
+  </section>
 </template>
